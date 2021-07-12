457e5f00d4edc4e32a71e132f3075aa63bb63fcf
Xianhui Zhu <xz2715@nyu.edu>
2021-07-12 15:19:21 -0400

    handle user input change

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UserSignupPage = () => {
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [cpassword, setCpassword] = useState('')
+
     return(
         <div>
             <h1>Sign Up</h1>
             <div>
-                <input placeholder="Your display name"/>
+                <input placeholder="Your display name"
+                value = {name}
+                onChange = {(input) => {
+                    setName(input.target.value)
+                }}/>
             </div>
             <div>
-                <input placeholder="Your username"/>
+                <input placeholder="Your username" 
+                value = {username} 
+                onChange = {(input)=> {
+                    setUsername(input.target.value)
+                }}/>
             </div>
             <div>
-                <input placeholder="Your password" type="password"/>
+                <input placeholder="Your password" type="password" 
+                value = {password}
+                onChange = {(input)=> {
+                    setPassword(input.target.value)
+                }}/>
             </div>
             <div>
-                <input placeholder="Repeat Your password" type="password"/>
+                <input placeholder="Repeat Your password" type="password"
+                value = {cpassword}
+                onChange = {(input)=> {
+                    setCpassword(input.target.value)
+                }}/>
             </div>
             <div>
                 <button>Sign Up</button>
