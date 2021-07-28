cd8a7f8db1b6646f41701dab1ea9f72d4f0e6b93
Xianhui Zhu <xz2715@nyu.edu>
2021-07-28 00:56:10 -0400

    return id of current user

--- backend/src/main/java/com/roaster/roaster/user/LoginController.java
+++ backend/src/main/java/com/roaster/roaster/user/LoginController.java
@@ -1,17 +1,24 @@
 package com.roaster.roaster.user;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.roaster.roaster.shared.CurrentUser;
 
 @RestController
 public class LoginController {
 	
 	@PostMapping("/api/1.0/login")
 	@CrossOrigin(origins = "http://localhost:3000")
-	void handleLogin() {
-		
+	Map<String, Object> handleLogin(@CurrentUser User loggedInUser) {
+		return Collections.singletonMap("id", loggedInUser.getId()); 
 	}
 	
 	// request is not at the stage of being passed to controller so use internal error forwarding mechanisms
--- backend/src/test/java/com/roaster/roaster/LoginControllerTest.java
+++ backend/src/test/java/com/roaster/roaster/LoginControllerTest.java
@@ -2,6 +2,8 @@ package com.roaster.roaster;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Map;
+
 import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
@@ -76,11 +80,20 @@ public class LoginControllerTest {
 	
 	@Test
 	public void postLogin_withValidCredentials_receiveOk() {
-		User user = TestUtil.createValidUser(); 
-		userService.save(user); 
+		userService.save(TestUtil.createValidUser()); 
 		authenticate(); 
 		ResponseEntity<Object> response = login(Object.class) ;
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK); 
+	}
+	
+	@Test
+	public void postLogin_withValidCredentials_receiveLoggedInUserId() {
+		User inDB = userService.save(TestUtil.createValidUser());
+		authenticate(); 
+		ResponseEntity<Map<String, Object>> response = login(new ParameterizedTypeReference<Map<String, Object>>(){});
+		Map<String, Object> body = response.getBody(); 
+		Integer id = (Integer) body.get("id"); 
+		assertThat(id).isEqualTo(inDB.getId());
 	}
 	
 	
@@ -91,5 +104,9 @@ public class LoginControllerTest {
 	
 	public <T> ResponseEntity<T> login(Class<T> responseType){
 		return testRestTemplate.postForEntity(API_1_0_LOGIN, null, responseType); 
+	}
+	
+	public <T> ResponseEntity<T> login(ParameterizedTypeReference<T> responseType){
+		return testRestTemplate.exchange(API_1_0_LOGIN, HttpMethod.POST, null, responseType); 
 	}
 }
