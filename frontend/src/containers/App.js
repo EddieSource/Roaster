c081a6c8613c9644ccec952ee46564a7034667ca
Xianhui Zhu <xz2715@nyu.edu>
2021-07-29 12:50:19 -0400

    add raw version of routers

---
+++ frontend/src/containers/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import * as apiCalls from "../api/apiCalls";
+import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
+import UserSignupPage from "../pages/UserSignupPage";
+import UserPage from "../pages/UserPage";
+import { Route, Switch } from "react-router-dom";
+
+const actions = {
+  postSignup: apiCalls.signup,
+  postLogin: apiCalls.login,
+};
+
+function App() {
+  // switch: used for provide single route
+  // exact: used when the router search for the exact path matching to be rendered(not from the root path matching)
+  return (
+    <div>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={UserSignupPage} />
+          <Route exact path="/:username" component={UserPage} />
+        </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div data-testid="homepage" />;
+};
+
+export default HomePage;
