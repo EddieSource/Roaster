3c73a70be2044bb51fedf3751b55d916cd67d7cf
Xianhui Zhu <xz2715@nyu.edu>
2021-08-01 16:23:24 -0400

    initialize redux

---
+++ frontend/src/redux/authReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  id: 0,
+  username: "",
+  image: "",
+  password: "",
+  isLoggedIn: false,
+};
+const authReducer = (state = initialState, action) => {
+  return state;
+};
+
+export default authReducer;
