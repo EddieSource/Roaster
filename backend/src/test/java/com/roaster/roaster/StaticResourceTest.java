a0dba9160823d5f64aca172c5f932b5d7c0ccec4
Xianhui Zhu <xz2715@nyu.edu>
2021-08-04 15:06:57 -0400

    enable caching of static resources

--- backend/src/test/java/com/roaster/roaster/StaticResourceTest.java
+++ backend/src/test/java/com/roaster/roaster/StaticResourceTest.java
@@ -1,16 +1,24 @@
 package com.roaster.roaster;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import com.roaster.roaster.configuration.AppConfiguration;
 
@@ -21,6 +29,9 @@ public class StaticResourceTest {
 	
 	@Autowired
 	AppConfiguration appConfiguration; 
+	
+	@Autowired
+	MockMvc mockMvc;
 	
 	@Test
 	public void checkStaticFolder_whenAppIsInitialized_uploadFolderMustExist() {
@@ -46,4 +57,57 @@ public class StaticResourceTest {
 		
 	}
 	
+	@Test
+	public void getStaticFile_whenImageExistInProfileUploadFolder_receiveOk() throws Exception {
+		String fileName = "profile-picture.png";
+		File source = new ClassPathResource("profile.png").getFile();
+		
+		File target = new File(appConfiguration.getFullProfileImagesPath() + "/" + fileName);
+		FileUtils.copyFile(source, target);
+		
+		mockMvc.perform(get("/images/"+appConfiguration.getProfileImagesFolder()+"/"+fileName)).andExpect(status().isOk());
+		
+	}
+	
+	@Test
+	public void getStaticFile_whenImageExistInAttachmentFolder_receiveOk() throws Exception {
+		String fileName = "profile-picture.png";
+		File source = new ClassPathResource("profile.png").getFile();
+		
+		File target = new File(appConfiguration.getFullAttachmentsPath() +  "/" + fileName);
+		FileUtils.copyFile(source, target);
+		
+		mockMvc.perform(get("/images/"+appConfiguration.getAttachmentsFolder()+"/"+fileName)).andExpect(status().isOk());
+		
+	}
+	
+	
+	@Test
+	public void getStaticFile_whenImageDoesNotExist_receiveNotFound() throws Exception {
+		mockMvc.perform(get("/images/"+appConfiguration.getAttachmentsFolder()+"/there-is-no-such-image.png"))
+			.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void getStaticFile_whenImageExistInAttachmentFolder_receiveOkWithCacheHeaders() throws Exception {
+		String fileName = "profile-picture.png";
+		File source = new ClassPathResource("profile.png").getFile();
+		
+		File target = new File(appConfiguration.getFullAttachmentsPath() + "/" + fileName);
+		FileUtils.copyFile(source, target);
+		
+		MvcResult result = mockMvc.perform(get("/images/"+appConfiguration.getAttachmentsFolder()+"/"+fileName)).andReturn();
+		
+		String cacheControl = result.getResponse().getHeaderValue("Cache-Control").toString();
+		// cache file for a year
+		assertThat(cacheControl).containsIgnoringCase("max-age=31536000");
+		
+	}
+	
+	
+	@AfterEach
+	public void cleanup() throws IOException {
+		FileUtils.cleanDirectory(new File(appConfiguration.getFullProfileImagesPath()));
+		FileUtils.cleanDirectory(new File(appConfiguration.getFullAttachmentsPath()));
+	}
 }
