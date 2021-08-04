314ef24afcb0b1a32ef636f59cb54206bdcd5d78
Xianhui Zhu <xz2715@nyu.edu>
2021-08-04 17:38:01 -0400

    implement file upload on frontend

--- frontend/src/components/ProfileImageWithDefault.js
+++ frontend/src/components/ProfileImageWithDefault.js
@@ -3,15 +3,18 @@ import defaultPicture from "../assets/profile.png";
 
 const ProfileImageWithDefault = (props) => {
   let imageSource = defaultPicture;
+  console.log("1stImageSource", imageSource);
   if (props.image) {
     imageSource = `/images/profile/${props.image}`;
   }
+  console.log("2ndImageSource", imageSource);
   return (
     //eslint-disable-next-line
     <img
       {...props}
       src={props.src || imageSource}
       onError={(event) => {
+        console.log("errr");
         event.target.src = defaultPicture;
         // target will give us the event's component
       }}
