9aa32613b64e18613c14a1014b6cb5a61d642418
Xianhui Zhu <xz2715@nyu.edu>
2021-08-06 14:04:21 -0400

    connect roast submit view to redux

--- frontend/src/components/RoastSubmit.js
+++ frontend/src/components/RoastSubmit.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import ProfileImageWithDefault from "./ProfileImageWithDefault";
+import { connect } from "react-redux";
 
-const RoastSubmit = () => {
+const RoastSubmit = (props) => {
   return (
     <div className="card d-flex flex-row p-1">
       <ProfileImageWithDefault
         className="rounded-circle m-1"
         width="32"
         height="32"
+        image={props.loggedInUser.image}
       />
       <div className="flex-fill">
         <textarea className="form-control w-100" rows={1} />
@@ -18,4 +20,9 @@ const RoastSubmit = () => {
 
 // RoastSubmit.defaultProps = defaultProps;
 
-export default RoastSubmit;
+const mapStateToProps = (state) => {
+  return {
+    loggedInUser: state,
+  };
+};
+export default connect(mapStateToProps)(RoastSubmit);
--- frontend/src/components/RoastSubmit.spec.js
+++ frontend/src/components/RoastSubmit.spec.js
@@ -1,18 +1,46 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import RoastSubmit from "./RoastSubmit";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import authReducer from "../redux/authReducer";
+
+const defaultState = {
+  id: 1,
+  username: "user1",
+  displayName: "display1",
+  image: "profile1.png",
+  password: "P4ssword",
+  isLoggedIn: true,
+};
+
+let store;
+
+const setup = (state = defaultState) => {
+  store = createStore(authReducer, state);
+  return render(
+    <Provider store={store}>
+      <RoastSubmit />
+    </Provider>
+  );
+};
 
 describe("RoastSubmit", () => {
   describe("Layout", () => {
     it("has textarea", () => {
-      const { container } = render(<RoastSubmit />);
+      const { container } = setup();
       const textArea = container.querySelector("textarea");
       expect(textArea).toBeInTheDocument();
     });
     it("has image", () => {
-      const { container } = render(<RoastSubmit />);
+      const { container } = setup();
       const image = container.querySelector("img");
       expect(image).toBeInTheDocument();
+    });
+    it("displays user image", () => {
+      const { container } = setup();
+      const image = container.querySelector("img");
+      expect(image.src).toContain("/images/profile/" + defaultState.image);
     });
   });
 });
