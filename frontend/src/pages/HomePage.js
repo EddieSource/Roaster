aeb00eca00333f24d4af63b58853e486fcebf386
Xianhui Zhu <xz2715@nyu.edu>
2021-08-06 23:32:57 -0400

    display roast post view only when the user is logged in

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import UserList from "../components/UserList";
 import RoastSubmit from "../components/RoastSubmit";
+import { connect } from "react-redux";
 
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <div data-testid="homepage">
       <div className="row">
         <div className="col-8">
-          <RoastSubmit />
+          {props.loggedInUser.isLoggedIn && <RoastSubmit />}
         </div>
         <div className="col-4">
           <UserList />
@@ -17,4 +18,9 @@ const HomePage = () => {
   );
 };
 
-export default HomePage;
+const mapStateToProps = (state) => {
+  return {
+    loggedInUser: state,
+  };
+};
+export default connect(mapStateToProps)(HomePage);
