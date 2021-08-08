7f0046e6a7bbfb33dd60e81601ebdbaa2fadfd17
Xianhui Zhu <xz2715@nyu.edu>
2021-08-08 16:17:43 -0400

    create entity relationship between roast and its attachment

--- backend/src/main/java/com/roaster/roaster/file/FileAttachment.java
+++ backend/src/main/java/com/roaster/roaster/file/FileAttachment.java
@@ -5,8 +5,11 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.roaster.roaster.roast.Roast;
 
 import lombok.Data;
 
@@ -23,4 +26,7 @@ public class FileAttachment {
 	private String name;
 	
 	private String fileType; 
+	
+	@OneToOne
+	private Roast roast; 
 }
